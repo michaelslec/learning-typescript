b4218582592a23d17eb68d7f5a42079c8aecc99e
Michael LeCompte <michaelslec98@gmail.com>
2021-03-02 22:27:11 -0800

    feat: exploring enums

--- project/app.ts
+++ project/app.ts
@@ -1,13 +1,30 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Michael",
+//   age: 22,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Michael",
   age: 22,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 let favoriteActivites: string[];
@@ -18,3 +35,5 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
 }
+
+if (person.role === Role.ADMIN) console.log("is admin");
