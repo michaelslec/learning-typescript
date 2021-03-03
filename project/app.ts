d33f12b757b12af54351996b14834878f28fa138
Michael LeCompte <michaelslec98@gmail.com>
2021-03-02 22:41:57 -0800

    feat: exploring type aliases

--- project/app.ts
+++ project/app.ts
@@ -1,5 +1,7 @@
-function combine(n1: number | string, n2: number | string) {
-  let result: number | string;
+type Combinable = number | string;
+
+function combine(n1: Combinable, n2: Combinable) {
+  let result: Combinable;
 
   if (typeof n1 === "number" && typeof n2 === "number") result = n1 + n2;
   else result = n1.toString() + n2.toString();
