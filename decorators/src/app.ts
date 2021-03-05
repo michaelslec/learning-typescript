42add7d879e2479f03459f27a8c738749efad2ca
Michael LeCompte <michaelslec98@gmail.com>
2021-03-05 05:52:13 -0800

    feat: property, accessor/mutator, parameter decorators

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -32,14 +32,50 @@ function Log(target: any | typeof Product, propertyName: string | Symbol) {
   console.log(target, propertyName);
 }
 
+function Log2(
+  target: Product | typeof Product,
+  name: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: Product | typeof Product,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(
+  target: Product | typeof Product,
+  name: string | Symbol,
+  position: number
+) {
+  console.log("Parameter decorator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
 class Product {
   @Log
   title: string;
+  private _price: number;
 
-  constructor(title: string, private _price: number) {
+  constructor(title: string, _price: number) {
     this.title = title;
+    this._price = _price;
   }
 
+  @Log2
   set price(val: number) {
     if (val > 0) this._price = val;
     else throw new Error("Invalid price - should be positive");
@@ -49,7 +85,8 @@ class Product {
     return this._price;
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this.price * (1 + tax);
   }
 }
