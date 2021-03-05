3688a5382762473ff61cb19bb6f7dd57e2d1da63
Michael LeCompte <michaelslec98@gmail.com>
2021-03-05 03:07:56 -0800

    feat: intro to promises

---
+++ generics/src/app.ts
@@ -0,0 +1,11 @@
+// const names: Array<string> = [];
+
+// const promise: Promise<string> = new Promise((resolve, _) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
+
+// promise.then((data) => {
+//   data.split(" ");
+// });
