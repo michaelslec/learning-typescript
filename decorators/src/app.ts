1a23abb93b0ef2a30f147b87d3c1673253048e4a
Michael LeCompte <michaelslec98@gmail.com>
2021-03-05 05:08:43 -0800

    feat: WithTemplate decorator

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -5,7 +5,15 @@ function Logger(logString: string) {
   };
 }
 
-@Logger("LOGGING - PERSON")
+function WithTemplate(template: string, hookId: string) {
+  return function (_: Function) {
+    const hookEl = document.getElementById(hookId);
+    if (hookEl !== null) hookEl.innerHTML = template;
+  };
+}
+
+// @Logger("LOGGING - PERSON")
+@WithTemplate("<h1>Hello, App!</h1>", "app")
 class Person {
   name = "max";
 
