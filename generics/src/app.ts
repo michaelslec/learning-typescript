bbdfa52dc10ef4aee652fd87ce533a453fcfb50f
Michael LeCompte <michaelslec98@gmail.com>
2021-03-05 04:17:44 -0800

    feat: object constraints

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -9,3 +9,10 @@
 // promise.then((data) => {
 //   data.split(" ");
 // });
+
+function merge<T extends object, U extends object>(a: T, b: U) {
+  return Object.assign(a, b);
+}
+
+const mergedObj = merge({ name: "Michael" }, { age: 22 });
+console.log(mergedObj.name);
