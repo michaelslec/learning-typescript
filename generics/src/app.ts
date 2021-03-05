f139d0a8e64358229acb24fc2fe47f0ebaa1b340
Michael LeCompte <michaelslec98@gmail.com>
2021-03-05 04:29:56 -0800

    feat: keyof

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -34,3 +34,12 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 }
 
 console.log(countAndDescribe(["Sports", "Cooking"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+console.log(extractAndConvert({ name: "Michael" }, "name"));
