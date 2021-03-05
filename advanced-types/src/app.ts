21731b187f32e80d7a90d05effb3745cc74d9a08
Michael LeCompte <michaelslec98@gmail.com>
2021-03-05 01:35:40 -0800

    feat: function overloads

--- advanced-types/src/app.ts
+++ advanced-types/src/app.ts
@@ -20,12 +20,17 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable | Numeric;
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string")
     return a.toString() + b.toString();
 
   return a + b;
 }
+
+const result = add("Michael", " LeCompte");
+result.split(" ");
 
 type UnknownEmployee = Employee | Admin;
 
