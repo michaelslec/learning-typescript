25994ff716572f333f0d18a2e67d94b0984909d5
Michael LeCompte <michaelslec98@gmail.com>
2021-03-05 04:23:07 -0800

    feat: generic constraint with interface

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -16,3 +16,19 @@ function merge<T extends object, U extends object>(a: T, b: U) {
 
 const mergedObj = merge({ name: "Michael" }, { age: 22 });
 console.log(mergedObj.name);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+  let descriptionText = "Got no value.";
+
+  if (element.length == 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements";
+  }
+
+  return [element, descriptionText];
+}
