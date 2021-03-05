978002edd4e5ceba421f65f325b6ff53c3d66e96
Michael LeCompte <michaelslec98@gmail.com>
2021-03-05 01:10:22 -0800

    feat: learning intersections!

---
+++ advanced-types/src/app.ts
@@ -0,0 +1,21 @@
+type Admin = {
+  name: string;
+  priveleges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Michael",
+  priveleges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable | Numeric;
