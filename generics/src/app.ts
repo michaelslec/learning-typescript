98f41f1f3ee9a83c35872bcb613f9fe3f4e0584e
Michael LeCompte <michaelslec98@gmail.com>
2021-03-05 04:35:04 -0800

    feat: generic class

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -43,3 +43,25 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: "Michael" }, "name"));
+
+class DataStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Michael");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
